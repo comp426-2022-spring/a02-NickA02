2bfa7bcbb38aacb59f68f23bc2a3b7a8a1273306
NickA02 <69744811+NickA02@users.noreply.github.com>
2022-03-01 15:03:24 -0500

    the last one was right but the error message wasnt right?

--- guess-flip.js
+++ guess-flip.js
@@ -3,6 +3,8 @@ import {flipACoin} from "./modules/coin.mjs"
 
 const argv = minimist(process.argv.slice(2))
 
+if (argv.call == null) console.error("Error: no input.\nUsage: node guess-flip --call=[heads|tails]")
+
 if (argv.call == "heads" || argv.call == "tails") {
     console.log(flipACoin(argv.call))
-}else console.error("Error: no input.\nUsage: node guess-flip --call=[heads|tails]")
+}else console.error("Error: invalid input.\nUsage: node guess-flip --call=[heads|tails]")
