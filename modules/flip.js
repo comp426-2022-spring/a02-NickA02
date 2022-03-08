9c8e3dc2895201e8b5ee2e0503b941ca51159322
NickA02 <69744811+NickA02@users.noreply.github.com>
2022-03-08 15:04:18 -0500

    Moved files to Module Folder

---
+++ modules/flip.js
@@ -0,0 +1,5 @@
+// Import the coinFlip function from your coin.mjs file
+import { coinFlip, countFlips, flipACoin } from "./coin.mjs";
+// Call the coinFlip function and put the return into STDOUT
+
+console.log(coinFlip())
