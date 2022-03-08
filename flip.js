70d9f685fa2506d1fe17b9646d660c905c74f7cf
NickA02 <69744811+NickA02@users.noreply.github.com>
2022-03-08 15:06:08 -0500

    Oops that broke it...

--- flip.js
+++ flip.js
@@ -1,5 +1,5 @@
 // Import the coinFlip function from your coin.mjs file
-import { coinFlip, countFlips, flipACoin } from "./coin.mjs";
+import { coinFlip, countFlips, flipACoin } from "./modules/coin.mjs";
 // Call the coinFlip function and put the return into STDOUT
 
 console.log(coinFlip())
